d0c34d57df0cc5b7a04b72c1f8414df01a6235c2
Johan Herrera <johanherrera20000@gmail.com>
2022-10-26 20:23:53 -0400

    The login page is responsive and have validations, I have to work on the server for create the user

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO characters (tier, name, attack, defense, sacrifice)
+VALUES
+    e= 6, d=8, c=10, b=12 a =14
+    ('E', 'offCenterDiv', 5, 1, 0),
+    ('E', 'Jester', 4, 2, 0),
+    ('E', 'CTRLALTDELETE', 3, 3, 0),
+    ('D', 'StackOverflowing', 6, 2, 0),
+    ('D', 'forEach', 7, 1, 0),
+    ('D', 'Terminal', 4, 4, 0),
+    ('D', 'Googlefu', 5, 3, 0),
+    ('C', 'Gitsome', 5, 5, 0),
+    ('C', 'GrimRepo', 8, 2, 0),
+    ('C', 'Gitbasher', 2, 8, 0),
+    ('C', 'CascadingSamuraiStars', 7, 3, 0),
+    ('C', 'ErrorCatcher', 1, 9, 0),
+    ('C', 'BranchSwitcher', 3, 7, 0),
+    ('C', 'FourOhFour', 4, 6, 0),
+    ('C', 'Refactorer', 6, 4, 0),
+    ('C', 'CTRLV', 9, 1, 0),
+    ('C', 'Iterator', 10, 0, 1),
+    ('C', 'Bootstrapped', 0, 10, 1),
+    ('B', 'destroyEnemy(you)', 8, 4, 1),
+    ('B', 'DeathNode', 9, 3, 1),
+    ('B', 'EventListener', 2, 10, 1),
+    ('B', 'JSONFoorheees', 11, 1, 2),
+    ('A', 'FatalError', 9, 5, 2),
+    ('A', 'TheCompiler', 12, 2, 3),
+    ('A', 'FullyStacked', 13, 1, 4);
